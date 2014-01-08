2ca7c449a256d62e47d351d394248805e232118a
krishnasuburayalu <s.krishnaraj@gmail.com>
2014-01-08 17:38:53 +0530

    initial checkin

---
+++ admin_user.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 08, 2014 at 11:26 AM
+-- Server version: 5.5.34-0ubuntu0.13.10.1
+-- PHP Version: 5.5.3-1ubuntu2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `magento`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_user`
+--
+
+CREATE TABLE IF NOT EXISTS `admin_user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'User ID',
+  `firstname` varchar(32) DEFAULT NULL COMMENT 'User First Name',
+  `lastname` varchar(32) DEFAULT NULL COMMENT 'User Last Name',
+  `email` varchar(128) DEFAULT NULL COMMENT 'User Email',
+  `username` varchar(40) DEFAULT NULL COMMENT 'User Login',
+  `password` varchar(100) DEFAULT NULL COMMENT 'User Password',
+  `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'User Created Time',
+  `modified` timestamp NULL DEFAULT NULL COMMENT 'User Modified Time',
+  `logdate` timestamp NULL DEFAULT NULL COMMENT 'User Last Login Time',
+  `lognum` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'User Login Number',
+  `reload_acl_flag` smallint(6) NOT NULL DEFAULT '0' COMMENT 'Reload ACL',
+  `is_active` smallint(6) NOT NULL DEFAULT '1' COMMENT 'User Is Active',
+  `extra` text COMMENT 'User Extra Data',
+  `rp_token` text COMMENT 'Reset Password Link Token',
+  `rp_token_created_at` timestamp NULL DEFAULT NULL COMMENT 'Reset Password Link Token Creation Date',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UNQ_ADMIN_USER_USERNAME` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Admin User Table' AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `admin_user`
+--
+
+INSERT INTO `admin_user` (`user_id`, `firstname`, `lastname`, `email`, `username`, `password`, `created`, `modified`, `logdate`, `lognum`, `reload_acl_flag`, `is_active`, `extra`, `rp_token`, `rp_token_created_at`) VALUES
+(1, 'krish', 'raj', 'admin@abc.com', 'admin', '0861d8f36ad3ba7acd4ef884da6bfabf:pkxqlWvv49IvklBXrOj9gYn0ndlCQNeB', '2014-01-08 05:56:06', '2014-01-08 05:56:06', NULL, 0, 0, 1, 'N;', NULL, NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+e53f3a2b9372babb593ed61c119c5ee1:El
